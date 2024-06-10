151e62dca10d74a0fdc566cbca7042d5e2ae6ec7
ravenyx <rizkirevan67@gmail.com>
2024-06-10 12:44:27 +0700

    Revisi dikit

--- FileSystem.java
+++ FileSystem.java
@@ -1,59 +1,82 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class FileSystem {
+public class FileSystem 
+{
     private Directory root;
     private Directory current;
     private long totalSpace = 100_000_000;
     private long freeSpace = totalSpace;
 
-    public FileSystem() {
+    public FileSystem() 
+    {
         root = new Directory("TesOS", null);
         current = root;
     }
 
-    public void mkdir(String name) {
-        if (current.hasChild(name)) {
+    public void mkdir(String name) 
+    {
+        if (current.hasChild(name)) 
+        {
             System.out.println("Directory already exists");
-        } else {
+        } 
+        else 
+        {
             Directory newDir = new Directory(name, current);
             current.addChild(name, newDir);
         }
     }
 
-    public void cd(String name) {
-        if ("/".equals(name)) {
+    public void cd(String name) 
+    {
+        if ("/".equals(name)) 
+        {
             current = root;
-        } else if ("..".equals(name)) {
-            if (current.getParent() != null) {
+        } 
+        else if ("..".equals(name)) 
+        {
+            if (current.getParent() != null) 
+            {
                 current = current.getParent();
-            } else {
+            } 
+            else 
+            {
                 System.out.println("Already at root directory");
             }
-        } else if (current.hasChild(name)) {
+        } 
+        else if (current.hasChild(name)) 
+        {
             current = current.getChild(name);
-        } else {
+        } 
+        else 
+        {
             System.out.println("Directory not found");
         }
     }
 
-    public void ls() {
-        for (String dirName : current.getChildren().keySet()) {
-            System.out.println(dirName + " <DIR>");
+    public void ls() 
+    {
+        for (String dirName : current.getChildren().keySet()) 
+        {
+            System.out.println(dirName + "      <DIR>");
         }
-        for (String fileName : current.getFiles().keySet()) {
-            System.out.println(fileName);
+        for (String fileName : current.getFiles().keySet()) 
+        {
+            System.out.println(fileName + "     <FILE>");
         }
     }
 
-    public void createFile(String name) {
-        if (current.hasFile(name)) {
+    public void createFile(String name) 
+    {
+        if (current.hasFile(name)) 
+        {
             System.out.println("File already exists");
             return;
         }
 
         int size = (int) (Math.random() * 2048) + 1; // Generate a random file size between 1 and 2048 bytes
-        if (size > freeSpace) {
+        if (size > freeSpace) 
+        {
             System.out.println("Not enough space to create the file");
             return;
         }
@@ -63,7 +86,8 @@ public class FileSystem {
         freeSpace -= size;
     }
 
-    public void dir() {
+    public void dir() 
+    {
         System.out.println(" Volume in drive TesOS is New Volume");
         System.out.println(" Volume Serial Number is 111B-222D");
         System.out.println("\n Directory of " + current.getFullPath());
@@ -72,11 +96,13 @@ public class FileSystem {
         System.out.printf("%-20s <DIR>            .\n", current.getFormattedCreationDate());
         System.out.printf("%-20s <DIR>            ..\n", current.getParent() != null ? current.getParent().getFormattedCreationDate() : current.getFormattedCreationDate());
 
-        for (Directory child : current.getChildren().values()) {
+        for (Directory child : current.getChildren().values()) 
+        {
             System.out.printf("%-20s <DIR>            %s\n", child.getFormattedCreationDate(), child.getName());
         }
 
-        for (File file : current.getFiles().values()) {
+        for (File file : current.getFiles().values()) 
+        {
             System.out.printf("%-20s          %7d %s\n", file.getFormattedCreationDate(), file.getSize(), file.getName());
         }
 
@@ -87,21 +113,29 @@ public class FileSystem {
         System.out.printf("               %d Dir(s) %13d bytes free\n", current.getChildren().size() + 2, freeSpace);
     }
 
-    public void rm(String name) {
-        if (current.hasFile(name)) {
+    public void rm(String name)
+    {
+        if (current.hasFile(name)) 
+        {
             File file = current.getFiles().remove(name);
             freeSpace += file.getSize();
             System.out.println("File deleted");
-        } else {
+        } 
+        else 
+        {
             System.out.println("File not found");
         }
     }
 
-    public void rmdir(String name) {
-        if (current.hasChild(name)) {
+    public void rmdir(String name)
+    {
+        if (current.hasChild(name))
+        {
             current.getChildren().remove(name);
             System.out.println("Directory deleted");
-        } else {
+        } 
+        else
+        {
             System.out.println("Directory not found");
         }
     }
@@ -110,32 +144,38 @@ public class FileSystem {
         System.out.println(current.getFullPath());
     }
 
-    public void cat(String name) {
-        if (current.hasFile(name)) {
+    public void cat(String name)
+    {
+        if (current.hasFile(name))
+        {
             System.out.println("Displaying contents of file: " + name);
-            // Assuming file has content, here we just print the file name as content
             System.out.println("Content of " + name);
-        } else {
+        }
+        else
+        {
             System.out.println("File not found");
         }
     }
 
-    public void help() {
+    public void help()
+    {
         System.out.println("Available commands:");
-        System.out.println("mkdir <name> - Create a new directory");
-        System.out.println("cd <name> - Change directory");
-        System.out.println("ls - List directories and files");
-        System.out.println("dir - Detailed list of directories and files");
-        System.out.println("create <name> - Create a new file");
-        System.out.println("rm <name> - Remove a file");
-        System.out.println("rmdir <name> - Remove a directory");
-        System.out.println("pwd - Print working directory");
-        System.out.println("cat <name> - Display file contents");
-        System.out.println("help - Display this help message");
-        System.out.println("exit - Exit the file system");
+        System.out.println("MKDIR <name>    Create a new directory");
+        System.out.println("CD <name>       Change directory");
+        System.out.println("LS              List directories and files");
+        System.out.println("DIR             Detailed list of directories and files");
+        System.out.println("CREATE <name>   Create a new file");
+        System.out.println("RM <name>       Remove a file");
+        System.out.println("RMDIR <name>    Remove a directory");
+        System.out.println("PWD             Print working directory");
+        System.out.println("CAT <name>      Display file contents");
+        System.out.println("HELP            Display this help message");
+        System.out.println("EXIT            Exit the file system");
+        System.out.println();
     }
 
-    public void prompt() {
+    public void prompt()
+    {
         System.out.print(current.getFullPath() + ">");
     }
 }
--- Main.java
+++ Main.java
@@ -1,40 +1,74 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        welcomeMessage();
         FileSystem fileSystem = new FileSystem();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while (true)
+        {
             fileSystem.prompt();
             String input = scanner.nextLine().trim();
-            if (input.equals("exit")) {
+            if (input.equals("exit"))
+            {
                 break;
-            } else if (input.startsWith("mkdir ")) {
+            } 
+            else if (input.startsWith("mkdir "))
+            {
                 fileSystem.mkdir(input.substring(6).trim());
-            } else if (input.startsWith("cd ")) {
+            } 
+            else if (input.startsWith("cd "))
+            {
                 fileSystem.cd(input.substring(3).trim());
-            } else if (input.equals("ls")) {
+            } 
+            else if (input.equals("ls"))
+            {
                 fileSystem.ls();
-            } else if (input.equals("dir")) {
+            }
+            else if (input.equals("dir"))
+            {
                 fileSystem.dir();
-            } else if (input.startsWith("create ")) {
+            }
+            else if (input.startsWith("create "))
+            {
                 fileSystem.createFile(input.substring(7).trim());
-            } else if (input.startsWith("rm ")) {
+            }
+            else if (input.startsWith("rm "))
+            {
                 fileSystem.rm(input.substring(3).trim());
-            } else if (input.startsWith("rmdir ")) {
+            }
+            else if (input.startsWith("rmdir "))
+            {
                 fileSystem.rmdir(input.substring(6).trim());
-            } else if (input.equals("pwd")) {
+            }
+            else if (input.equals("pwd"))
+            {
                 fileSystem.pwd();
-            } else if (input.startsWith("cat ")) {
+            }
+            else if (input.startsWith("cat "))
+            {
                 fileSystem.cat(input.substring(4).trim());
-            } else if (input.equals("help")) {
+            }
+            else if (input.equals("help"))
+            {
                 fileSystem.help();
-            } else {
+            }
+            else
+            {
                 System.out.println("Invalid command");
             }
         }
 
         scanner.close();
+    }
+
+    private static void welcomeMessage()
+    {
+        System.out.println();
+        System.out.println("Kelompok 1 Custom OS [Version 1.0.0]");
+        System.out.println("(c) 2024. All rights reserved.");
+        System.out.println();
     }
 }
